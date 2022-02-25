2594b0816413d9e8ac65e77fb688de9f7c96ae8d
lsscecilia <lsscecilia8888@gmail.com>
2022-02-26 03:43:16 +0800

    Add simple UI for android app + server

---
+++ README.md
@@ -0,0 +1,3 @@
+# IOT project
+
+This repo includes the app (client) and the server that is hosted on google cloud
---
+++ server/server.py
@@ -0,0 +1,60 @@
+import flask
+import io
+import string
+import time
+import os
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, jsonify, request
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+
+model = ResNet50(weights='imagenet')
+
+
+def prepare_image(img):
+    img = Image.open(io.BytesIO(img))
+    img = img.resize((224, 224))
+    img = np.array(img)
+    img = np.expand_dims(img, 0)
+    img = preprocess_input(img)
+    return img
+
+
+# def predict_result(img):
+#     return 1 if model.predict(img)[0][0] > 0.5 else 0
+
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def infer_image():
+    if 'file' not in request.files:
+        return "Please try again. The Image doesn't exist"
+    
+    file = request.files.get('file')
+
+    if not file:
+        return
+
+    img_bytes = file.read()
+    img = prepare_image(img_bytes)
+    preds = model.predict(img)
+
+    raw_pred = decode_predictions(preds, top=3)[0]
+    object_names = []
+    for i in raw_pred:
+        object_names.append(i[1])
+    print('Predicted:', object_names)
+    return ','.join(object_names)
+    
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Machine Learning Inference'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
