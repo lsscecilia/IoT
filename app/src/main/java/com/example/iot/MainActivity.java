2655689dc658bb44a5fdb5f62e8cce0cbab9146d
lsscecilia <lsscecilia8888@gmail.com>
2022-02-26 18:43:23 +0800

    Add android connection to server

--- app/src/main/java/com/example/iot/MainActivity.java
+++ app/src/main/java/com/example/iot/MainActivity.java
@@ -4,12 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +58,70 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 Bitmap bp = (Bitmap) data.getExtras().get("data");
                 imgCapture.setImageBitmap(bp);
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                File tempFile = null;
+                try {
+                    tempFile = File.createTempFile("my_app", ".jpg");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                OutputStream outFile = null;
+                try {
+                    outFile = new FileOutputStream(tempFile);
+                    bp.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                    outFile.flush();
+                    outFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+//                bmp.recycle();
+
+//                File tempFile = null;
+//                try {
+//                    tempFile = File.createTempFile("my_app", ".jpg");
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                Uri uri = Uri.fromFile(tempFile);
+//                data.putExtra(String.valueOf(byteArray), uri);
+//                System.out.println(tempFile);
+//
+                String URL = "http://34.87.129.213:5000/predict";
+                RequestParams rp = new RequestParams();
+
+                try {
+                rp.put("file", tempFile);
+                System.out.println(rp.toString());
+                } catch(FileNotFoundException e) {
+
+                }
+                System.out.println("send request");
+//                rp.add("username", "aaa"); rp.add("password", "aaa@123");
+
+                HttpUtils.post(URL, rp, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        // If the response is JSONObject instead of expected JSONArray
+                        Log.d("asd", "---------------- this is response : " + response);
+                        try {
+                            JSONObject serverResp = new JSONObject(response.toString());
+                        } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                        // Pull out the first event on the public timeline
+
+                    }
+                });
+
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             }
